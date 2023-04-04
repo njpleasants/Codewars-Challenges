8ba6d016fd4713da0da507f36439ce700d175d6b
njpleasants <njpleasants@gmail.com>
2023-04-03 20:49:43 -0400

    codewars challenge convert a boolean to a string

---
+++ CC_2023-04-03/app.js
@@ -0,0 +1,17 @@
+/*Convert a Boolean to a String 
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.*/
+
+function booleanToString(b){
+    if(b === true){
+      return 'true';
+    }else{
+      return'false';
+    }
+  }
+
+  // best practice option
+  function booleanToString(b){
+    return b ? 'true' : 'false';
+  }
