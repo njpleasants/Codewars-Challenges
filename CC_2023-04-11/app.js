2ec18944e1a183d3eb49f712a09f4a411102b20c
Nia Pleasants <njpleasants@gmail.com>
2023-04-11 16:37:32 -0400

    Adding Codewars challenge MakeUpperCase 8kyu

---
+++ CC_2023-04-11/app.js
@@ -0,0 +1,11 @@
+//MakeUpperCase
+//Write a function which converts the input string to uppercase.
+
+//P: will always be a string 
+//R: return string all in uppercase
+//E: const sentence = 'The quick brown fox jumps over the lazy dog.';
+//console.log(sentence.toUpperCase());
+//P:
+function makeUpperCase(str) {
+ return str.toUpperCase( );
+}
