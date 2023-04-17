2477786e4d8ff6d782c6191add4cfbeb76c022c3
Nia Pleasants <njpleasants@gmail.com>
2023-04-16 21:37:09 -0400

    Adding Codewars challenge You Can't Code Under Pressure #1 8kyu

---
+++ CC_2023-04-16/app.js
@@ -0,0 +1,12 @@
+//You Can't Code Under Pressure #1 8kyu
+//P: Will always be a number
+//R: return the double of an integer
+//E: num * 2
+//P:
+function doubleInteger(i) {
+        return i * 2;
+      }
+
+  //Other Codewars solutions
+
+  const doubleInteger = i => 2 * i;
