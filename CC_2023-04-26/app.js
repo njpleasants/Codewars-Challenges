7764f9b94c629e1023769392616803e3e8155bb4
Nia Pleasants <njpleasants@gmail.com>
2023-04-26 12:42:23 -0400

    Adding Codewars challenge Sentence Smash 8kyu

---
+++ CC_2023-04-26/app.js
@@ -0,0 +1,12 @@
+//Sentence Smash 8kyu
+//Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+//P: The array will not be empty, there should be no spaces at the beggining or end of the sentence
+//R: Return the array of words combined into a sentence
+//E: ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+//P: 
+function smash (word1, word2, word3) {
+    return arr.join(' ');
+ };
+
+ //Other Codewars solutions
+ const smash = words => words.join(' ');
