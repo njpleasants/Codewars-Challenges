581cb8cc56a9d5cf59d6caeebf4cd817ab963d4d
Nia Pleasants <njpleasants@gmail.com>
2023-05-02 18:42:02 -0400

    Adding Codewars challenge Convert a String to an Array 8kyu

---
+++ CC_2023-05-02/app.js
@@ -0,0 +1,12 @@
+//Convert a string to an array
+//Write a function to split a string and convert it into an array of words.
+//P: Will not have an empty string or array
+//R: return an array of words
+//E: "Robin Singh" ==> ["Robin", "Singh"]
+//P:
+function stringToArray(string){
+    return string.split(' ');
+}
+
+//Other Codewars solutions:
+const stringToArray = string => string.split(' ');
