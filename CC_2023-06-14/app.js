e0f3c80c062f430d125ce6c3680aba0cc86a385e
Nia Pleasants <njpleasants@gmail.com>
2023-06-14 18:55:13 -0400

    Adding Codewars challenge Jennys Secret Message 8kyu

---
+++ CC_2023-06-14/app.js
@@ -0,0 +1,28 @@
+//Jenny's Secret Message 8kyu
+//Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake. Can you help her?
+//P:
+//R:
+//E:
+//P:
+function greet(name){
+  if( name !== "Johnny"){
+    return "Hello, " + name + "!";} 
+  else if (name === "Johnny"){
+    return "Hello, my love!";
+  }
+}
+
+//Other Codewars solutions
+function greet(name){
+  return "Hello, " + (name == "Johnny" ? "my love" : name) + "!";
+}
+
+function greet(name){  
+  if(name === "Johnny")
+    return "Hello, my love!";
+  return "Hello, " + name + "!";
+}
+
+function greet(name){
+  return name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
+}
